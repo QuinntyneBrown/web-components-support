658209c58546c7eec563296a184efdfedbfa3cd6
Quinntyne Brown <quinntynebrown@gmail.com>
2016-12-10 10:03:13 -0500

    initial code commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+﻿import "core-js/es6";
+import "@webcomponents/custom-elements/custom-elements.min";
+
+class MyComponent extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = "<h1>Too much sauce!</h1>";
+    }
+}
+
+customElements.define("my-component", MyComponent);
